a3129dfb92e8f44e4defc84705051e1ca5ba1f99
Rory <G00422319@atu.ie>
2023-10-16 10:49:37 +0100

    added new class called calculator to subtract and multiply two numbers.

--- src/main/java/ie/atu/Calculator1.java
+++ src/main/java/ie/atu/Calculator1.java
@@ -9,6 +9,19 @@ public class Calculator1 {
     public static void main(String[] args) {
         subtract();
         add();
+        Multiple();
+    }
+
+    private static void Multiple() {
+        System.out.println("Please your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your second number :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The total is " + total);
     }
 
     private static void subtract() {
